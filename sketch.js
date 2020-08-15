b9050fc1e3582d810a6f861abb9130c98d319a5b
Nazrin P B <pbnazrin@gmail.com>
2020-08-16 00:58:15 +0530

    add platform

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
-
+    platform = new Ground(150,305,300,170)
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -43,6 +43,7 @@ function draw(){
     console.log(box2.body.position.x);
     console.log(box2.body.position.y);
     console.log(box2.body.angle);
+    platform.display();
     box1.display();
     box2.display();
     ground.display();
